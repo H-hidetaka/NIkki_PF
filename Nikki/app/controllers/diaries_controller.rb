be7289464af3ff91a31538ae12093caec16fc81d
H-hidetaka <sansanhidetaka@gmail.com>
2022-02-10 15:57:21 +0900

    [Add]フォーム入力時エラー1

--- Nikki/app/controllers/diaries_controller.rb
+++ Nikki/app/controllers/diaries_controller.rb
@@ -13,8 +13,6 @@ class DiariesController < ApplicationController
       redirect_to  new_diary_path, success: t('defaults.message.created', item: Diary.model_name.human)
     else
       redirect_to  new_diary_path, danger: t('defaults.message.not_created', item: Diary.model_name.human)
-      # flash.now[danger] = t('defaults.message.not_created',item: Diary.model_name.human)
-      # render :new
     end
   end
 
--- Nikki/app/controllers/user_sessions_controller.rb
+++ Nikki/app/controllers/user_sessions_controller.rb
@@ -5,7 +5,7 @@ class UserSessionsController < ApplicationController
 
   def create
     @user = login(params[:email], params[:password])
-    if @user.save
+    if @user
       redirect_back_or_to diaries_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
